c7413aba1e85b0041e82ee431263bbed33f3cd76
Samuel Vincent <samuel1.vincent101@gmail.com>
2017-04-01 21:11:58 -0400

    J'ai commit

---
+++ TP2ETU/Score.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TP2PROF
+{
+    public class Score
+    {
+        private static Score instance = null;
+        private int pills;
+        private int superPills;
+        private int ghostEaten;
+
+        private Score()
+        {
+            pills = 0;
+            superPills = 0;
+        }
+
+        public Score GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new Score();
+                return instance;
+            }
+            else
+            {
+                return instance;
+            }
+
+        }
+
+        public int Pills
+        {
+            get { return pills; }
+            set { pills = value; }
+        }
+
+        public int SuperPills
+        {
+            get {return superPills;}
+            set { superPills = value; }
+
+        }
+
+    }
+}
